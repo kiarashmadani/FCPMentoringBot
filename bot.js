d0387fa256c9ee87156d38b490b5c1f6fd8d579e
kiarashmadani <77398305+kiarashmadani@users.noreply.github.com>
2026-02-22 00:40:53 +0330

    bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,26 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
 
-const token = '8396112909:AAFj7U1FhMSkYR6lquaPBvhVOCovtIpSfts';
-const bot = new TelegramBot(token, { polling: true });
+const token = process.env.TOKEN;
+const url = process.env.RENDER_EXTERNAL_URL;
+
+const bot = new TelegramBot(token);
+const app = express();
+
+app.use(express.json());
+
+bot.setWebHook(`${url}/bot${token}`);
+
+app.post(`/bot${token}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
 
 bot.on('message', (msg) => {
     console.log(msg.chat.id);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server running...");
 });
