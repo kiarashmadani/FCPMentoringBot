f0513ca6c04f2ecd5ca51fe02530c9683e97dad1
kiarashmadani <kiarash.madani85@gmail.com>
2026-02-21 23:29:36 +0330

    first commit

--- bot.js
+++ bot.js
@@ -1,18 +1,7 @@
+const TelegramBot = require('node-telegram-bot-api');
 
 const token = '8396112909:AAFj7U1FhMSkYR6lquaPBvhVOCovtIpSfts';
-const TelegramBot = require('node-telegram-bot-api');
-const { HttpsProxyAgent } = require('https-proxy-agent'); // 👈 این مهمه
-
-const proxy = 'http://127.0.0.1:7890'; // پورت پراکسی‌ات
-
-const agent = new HttpsProxyAgent(proxy);
-
-const bot = new TelegramBot(token, {
-    polling: true,
-    request: {
-        agent: agent
-    }
-});
+const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
     console.log(msg.chat.id);
