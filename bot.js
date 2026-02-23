f1a4b07679baaa7b34d8bb7d1f272ed4a6bcb424
kiarashmadani <77398305+kiarashmadani@users.noreply.github.com>
2026-02-23 23:56:05 +0330

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("Bot is running 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TOKEN;
